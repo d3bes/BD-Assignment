0b5650e30f598231d5cf46e3308c6164a8ea6473
d3bes <abdo.mohamed6319@gmail.com>
2023-11-12 22:53:53 +0200

    addAuthentication

--- Assignment_API/Program.cs
+++ Assignment_API/Program.cs
@@ -1,3 +1,8 @@
+using Google.Apis.Auth.AspNetCore3;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +10,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+ builder.Services.AddMvc(); 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(op =>
+{
+    op.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();  //.WithOrigins("http://localhost:4200,")
+    });
+});
+
+
 var app = builder.Build();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,7 +32,40 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
+
+
+ void ConfigureServices(IServiceCollection services)
+{
+    
+
+    // This configures Google.Apis.Auth.AspNetCore3 for use in this app.
+    services
+        .AddAuthentication(o =>
+        {
+            // This forces challenge results to be handled by Google OpenID Handler, so there's no
+            // need to add an AccountController that emits challenges for Login.
+            o.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+            // This forces forbid results to be handled by Google OpenID Handler, which checks if
+            // extra scopes are required and does automatic incremental auth.
+            o.DefaultForbidScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+            // Default scheme that will handle everything else.
+            // Once a user is authenticated, the OAuth2 token info is stored in cookies.
+            o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddGoogleOpenIdConnect(options =>
+        {
+            options.ClientId = "758095339445-g0uree204ajoq08m610dvkiu60o02fio.apps.googleusercontent.com";
+            options.ClientSecret = "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";
+        });
+}
+      
+
 app.UseHttpsRedirection();
+   
+app.UseAuthentication();
 
 app.UseAuthorization();
 
